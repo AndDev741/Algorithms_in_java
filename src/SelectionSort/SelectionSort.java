88dcaceb869b76a65a69755998f65037fb3fbf41
anddev741 <anddesenvolved@gmail.com>
2025-03-27 08:25:42 +0000

    refactor: Optimized the old selection sort algorithm, now execture in-place

--- src/SelectionSort/SelectionSort.java
+++ src/SelectionSort/SelectionSort.java
@@ -1,41 +1,31 @@
 package SelectionSort;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class SelectionSort {
     public static void main(String[] args){
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        array.add(233);
-        array.add(2);
-        array.add(15);
-        array.add(355);
-        array.add(33);
-        System.out.println(selectionSort(array));
-    }
-
-    public static ArrayList<Integer> selectionSort(ArrayList<Integer> array){
-        ArrayList<Integer> newArray = new ArrayList<Integer>(array.size());
-        int size_of_array = array.size();
-        for(int i = 0; i < size_of_array; i++){
-            int index = findBiggestNumber(array);
-            newArray.add(array.remove(index));
+        int[] numbersList = {233, 2, 15, 355, 33};
+        int[] orderedList = selectionSort(numbersList);
+        for(int i = 0; i < orderedList.length; i++){
+            System.out.println(orderedList[i]);
         }
-        return newArray;
     }
 
-    public static int findBiggestNumber(ArrayList<Integer> array){
-        int biggest = array.get(0);
-        int biggestIndex = 0;
+    public static int[] selectionSort(int[] list){
+        for(int i = 0; i < list.length; i++){
+            int minIndex = i;
 
-        for(int i = 0; i < array.size(); i++){
-            if(array.get(i) > biggest){
-                biggest = array.get(i);
-                biggestIndex = i;
+            for(int j = i + 1; j < list.length; j++){
+                if(list[j] < list[minIndex]){
+                    minIndex = j;
+                    System.out.println("New minIndex: " + j);
+                }
             }
-        }
-        return biggestIndex;
-    }
 
+            int temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+
+        return list;
+    }
 
 }
