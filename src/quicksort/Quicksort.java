b5d224c31c0e1ce79e97f933919c334667b29f65
anddev741 <anddesenvolved@gmail.com>
2025-06-05 07:43:47 +0100

    feat: Improvements in quicksort

--- src/quicksort/Quicksort.java
+++ src/quicksort/Quicksort.java
@@ -1,45 +1,60 @@
 package quicksort;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Random;
 
 public class Quicksort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(5);
         arr.add(25);
         arr.add(2);
-        arr.add(8);
+        arr.add(-1);
         arr.add(7);
+        arr.add(0);
+        arr.add(25);
         arr.add(1);
-        List<Integer> ordenedResult = quicksort(arr);
+
+        Long startTime = System.nanoTime();
+
+        ArrayList<Integer> ordenedResult = quicksort(arr);
+
+        Long endTime = System.nanoTime();
+        Long durationInNanoseconds = endTime - startTime;
+        double durationInMilliseconds = durationInNanoseconds / 1_000_000.0;
+
+        System.out.println("Execution in milisseconds: " + durationInMilliseconds);
         System.out.println(ordenedResult);
     }
 
-    public static List quicksort(List<Integer> arr){
-        if(arr.size() < 2){
+    public static ArrayList<Integer> quicksort(ArrayList<Integer> arr) {
+        if (arr.size() < 2) {
             return arr;
         }
-        //recursive case
-        Integer pivot = arr.getFirst();
-        //All the numbers less than the pivot
-        List<Integer> lessThanPivot = new ArrayList<>();
-        //All the numbers bigger than the pivot
-        List<Integer> biggerThanPivot = new ArrayList<>();
+        // recursive case
+        Random random = new Random();
+        Integer pivotIndex = random.nextInt(arr.size());
+        Integer pivot = arr.get(pivotIndex);
 
-        for(int i = 1; i < arr.size(); i++){
-            if(arr.get(i) <= pivot){
+        // All the numbers less than the pivot
+        ArrayList<Integer> lessThanPivot = new ArrayList<>();
+        // All the numbers bigger than the pivot
+        ArrayList<Integer> biggerThanPivot = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(i == pivotIndex) continue;
+            if (arr.get(i) <= pivot) {
                 lessThanPivot.add(arr.get(i));
             } else {
                 biggerThanPivot.add(arr.get(i));
             }
         }
 
-        return (List) Stream.of(quicksort(lessThanPivot).stream(),
-                Stream.of(pivot),
-                quicksort(biggerThanPivot).stream())
-                .flatMap(s -> s).collect(Collectors.toList());
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(quicksort(lessThanPivot));
+        result.add(pivot);
+        result.addAll(quicksort(biggerThanPivot));
+
+        return result;
     }
 }
